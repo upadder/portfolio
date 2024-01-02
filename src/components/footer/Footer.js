9888e607402795ccada393aedbe5d2893cd365b0
upadhyay.prakash1999@gmail.com <u_pad@1369>
2024-01-02 14:17:14 -0500

    change theme

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,10 +9,8 @@ export default function Footer(props) {
     <div className="footer-div">
       <Fade>
         <p className="footer-text" style={{ color: props.theme.secondaryText }}>
-          Made with <span role="img">❤️</span> by {greeting.title}
-        </p>
-        <p>
-          Theme by developerFolio
+          Made with <span role="img">❤️</span> by {greeting.title} | Theme by
+          developerFolio
         </p>
         {/* <ToggleSwitch theme={props.theme} onToggle={props.onToggle}/> */}
       </Fade>
